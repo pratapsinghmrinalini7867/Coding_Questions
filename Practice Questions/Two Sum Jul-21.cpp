9ac530d7269f3e5a89209606c4b982090c7f7227
Mrinalini Pratap <57532977+pratapsinghmrinalini7867@users.noreply.github.com>
2021-07-21 14:54:36 +0530

    Add files via upload

---
+++ Practice Questions/Two Sum Jul-21.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int>a;
+        for(int i=0;i<nums.size();i++){
+            int remaining = target - nums[i];
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[j] == remaining){
+                    a.push_back(i);
+                    a.push_back(j);
+                    return(a);
+                }
+            }
+        }
+       return(a);
+    }
+};
